839da724c0e0415058395ced8024c7b3325745e1
Thunyatorn <58735442+Thunyatorn@users.noreply.github.com>
2020-06-15 17:52:42 +0700

    Add files via upload

---
+++ pattern_5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+main()
+{
+    int a,i,j;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++){
+        for(j=a-1;j>=i;j--){
+            printf("-");
+        }
+        for(j=1;j<=i;j++){
+            if(a%2==1){
+                if(j%2==1){
+                    printf("^");
+                }else if(j%2==0){
+                    printf("*");
+                }
+            }else if(a%2==0){
+                if(j%2==1){
+                    printf("*");
+                }else if(j%2==0){
+                    printf("^");
+                }
+            }
+        }
+    printf("\n");
+    }
+}
