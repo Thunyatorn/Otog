1cc26c2e266a86c6e18aacb40751c690066eaf57
Thunyatorn <58735442+Thunyatorn@users.noreply.github.com>
2020-06-15 18:04:41 +0700

    Update pattern_3.c

--- pattern_3.c
+++ pattern_3.c
@@ -2,11 +2,11 @@
 int main(){
     int n;
     scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i-1;j++){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
             printf("-");
         }
-        for(int j=n;j>=i;j--){
+        for(int j=n;j>i;j--){
             printf("*");
         }
         printf("\n");
