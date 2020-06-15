ac5096f4d302328be56f449dfc8887a4e36b9866
Thunyatorn <58735442+Thunyatorn@users.noreply.github.com>
2020-06-15 17:56:06 +0700

    Update pattern_1.c

--- pattern_1.c
+++ pattern_1.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
-    int n,i,j;
+    int n;
     scanf("%d",&n);
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
             printf("*");
         }
         printf("\n");
     }
+    return 0;
 }
