c9d1a7cd37be913dc96f7df5fc2e8b8b1b0c7049
Thunyatorn <58735442+Thunyatorn@users.noreply.github.com>
2020-06-19 00:10:41 +0700

    Update Plus1.c

--- Plus1.c
+++ Plus1.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 int n, m, x[1000][1000], sum=0;
-
 int main(){
-    int a, b, k, i, j;
+    int k, i, j;
     scanf("%d%d",&n,&m);
-    if(n<=1000&&m<=1000){
-    scanf("%d%d",&a,&b);
-    for(int i=0;i<n;i++){
+    int a, b;scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++){
         for(j=0;j<m;j++){
             scanf("%d",&x[i][j]);
         }
@@ -19,7 +17,4 @@ int main(){
     }
     sum=sum-x[a-1][b-1];
     printf("%d",sum);
-    }else{
-    return 0;
-    }
 }
